4f999a5da9d13697232bfe4bf62b1b03087ebda0
Alonbt <alonbt@gmail.com>
2013-12-30 11:32:48 +0200

    I got heroku to work

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+console.log('hello app');
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/dist');
+app.engine('html', require('ejs').renderFile);
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+app.use('/', express.static(__dirname + '/dist'));
 app.listen(port);
